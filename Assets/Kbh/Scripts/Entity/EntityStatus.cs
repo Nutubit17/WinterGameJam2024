eb9325e646361217d2344662b0672d43de1f3f7d
Nutubit17 <koobonhoo@gmail.com>
2025-03-30 03:06:41 -0400

    .

--- Assets/Kbh/Scripts/Entity/EntityStatus.cs
+++ Assets/Kbh/Scripts/Entity/EntityStatus.cs
@@ -4,50 +4,54 @@ using UnityEngine;
 
 public interface IEntity
 {
-   EntityStatus Status { get; set; }
+    EntityStatus Status { get; set; }
 }
 
 public interface IEntityComponent
 {
-   void Init(IEntity entity);
+    void Init(IEntity entity);
 }
 
 [System.Serializable]
 public class EntityStatus
 {
-   [field: SerializeField] public int MaxHp { get; private set; } = 5;
-   public int CurrentHp { get; private set; }
-   [field: Space(10)]
-   [field: SerializeField] public float Speed { get; private set; } = 5;
-   [field: Space(10)]
-   [field: SerializeField] public float MaxStamina { get; private set; } = 5;
-   public float CurrentStamina { get; private set; }
+    [field: SerializeField] public int MaxHp { get; private set; } = 5;
+    public int CurrentHp { get; private set; }
+
+    [field: Space(10)]
+    [field: SerializeField]
+    public float Speed { get; private set; } = 5;
+
+    [field: Space(10)]
+    [field: SerializeField]
+    public float MaxStamina { get; private set; } = 5;
+
+    public float CurrentStamina { get; private set; }
 
 
-   public void Init()
-   {
-      CurrentHp = MaxHp;
-      CurrentStamina = MaxStamina;
-   }
+    public void Init()
+    {
+        CurrentHp = MaxHp;
+        CurrentStamina = MaxStamina;
+    }
 
-   public void AddHp(int amount)
-   {
-      CurrentHp = Mathf.Clamp(CurrentHp + amount, 0, MaxHp);
-   }
+    public void AddHp(int amount)
+    {
+        CurrentHp = Mathf.Clamp(CurrentHp + amount, 0, MaxHp);
+    }
 
-   public void AddStamina(float amount)
-   {
-      CurrentStamina = Mathf.Clamp(CurrentStamina + amount, 0, MaxStamina);
-   }
+    public void AddStamina(float amount)
+    {
+        CurrentStamina = Mathf.Clamp(CurrentStamina + amount, 0, MaxStamina);
+    }
 
-   public void ExtentStamina(float amount)
-   {
-      MaxStamina += amount;
-   }
+    public void ExtentStamina(float amount)
+    {
+        MaxStamina += amount;
+    }
 
-   public void AddSpeed(float speed)
-   {
-      Speed += speed;
-   }
-   
+    public void AddSpeed(float speed)
+    {
+        Speed += speed;
+    }
 }
